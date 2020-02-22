cdbd423585751880abaeab728aa9f151a5db7d36
Cauê Tagliari <41552570+banzaibill7@users.noreply.github.com>
2020-02-22 09:23:34 -0300

    Update Card Display.cs

--- Useful-Scripts/Card Display.cs
+++ Useful-Scripts/Card Display.cs
@@ -78,7 +78,7 @@ void Start ()
   nameText.text = card.name;
   descriptionText.text = card.description;
   
-  artworkImage.sprit = card.artwork;
+  artworkImage.sprite = card.artwork;
   
   manaText.text = card.manaCost.ToString();
   attackText.text = card.attack.ToString();
